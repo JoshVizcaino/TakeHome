8a522bc29d02a55f1f17730004e02c4d811afa81
PlaysportJosh <66018549+PlaysportJosh@users.noreply.github.com>
2020-10-20 13:48:12 +0100

    added some files

---
+++ TakeHomeInterview/Assets/Code/UI/UIScreen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UIScreen : MonoBehaviour
+{
+    //------------------------------------------------------------------------------------
+    // Data
+    //------------------------------------------------------------------------------------
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
